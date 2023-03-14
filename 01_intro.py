cf5e9f5adde61e26f547b1ff84d39f58fb131a77
Victor Bernal <victoralb29@hotmail.com>
2023-03-13 19:07:34 -0600

    tarea intro.py

---
+++ 01_intro.py
@@ -0,0 +1,15 @@
+print("Hola mundo")
+
+#Tarea 1 Realizar operaciones y tipos dados
+print(2+8) #El resultado es 10
+print(5*6) #El resultado es 30
+print(4/3) #El resultado es 1.33
+print(4-10) #El resultado es -6
+print(9<11) #El numero mayor
+print(9>11) #El numero menor
+
+## Tipo de variables obtenidas en los sistemas operacionales
+print(type(2+8)) #tipo entero-int
+print(type(5+6))  #Tipo int
+print(type(4/3))  #Tipo flotante float
+print(type(4-10))  #Tipo entero
